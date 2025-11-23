cd7ca4c8207d44e38a48534d0b3a22ce4dbe81fd
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-11-23 16:08:56 +0530

    commit

---
+++ Hm6.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Hm6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     HashMap<Integer, String> hm=new HashMap<Integer, String>();
+	       
+	       hm.put(1, "Red");
+	       hm.put(2, "Green");
+	       hm.put(3, "Black");
+	       hm.put(4, "White");
+	       hm.put(5, "Blue");
+	       
+	       System.out.println("Og Map : "+hm);
+	       
+	       System.out.println("clone Map : "+hm.clone());
+	}
+
+}
