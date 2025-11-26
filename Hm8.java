ac1464b54cfd020994cb93bece22a816228cd4ed
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-11-26 18:59:04 +0530

    commit

---
+++ Hm8.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Hm8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 HashMap<Integer, String> hm=new HashMap<Integer, String>();
+	       
+	       hm.put(1, "Red");
+	       hm.put(2, "Green");
+	       hm.put(3, "Black");
+	       hm.put(4, "White");
+	       hm.put(5, "Blue");
+	       
+	      System.out.println( "Value for key 3 "+hm.get(3));
+	}
+
+}
